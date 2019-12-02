1e0ca77b1c1a2d16b327a0386de7fbbee1a642f1
wolfsinem <sinem.ertem@student.hu.nl>
2019-12-02 15:31:22 +0100

    added all tests

--- test_calc.py
+++ test_calc.py
@@ -4,14 +4,29 @@ import calc
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-        result = calc.add(10,5)
-        self.assertEqual(result,15)
+        self.assertEqual(calc.add(10,5),15)
+        self.assertEqual(calc.add(-1,1),0)
+        self.assertEqual(calc.add(-1,-1),-2)
         #run python -m unittest test_calc
         #outcome: OK --> test is geslaagd 
+        
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(10,5),5)
+        self.assertEqual(calc.subtract(-1,1),-2)
+        self.assertEqual(calc.subtract(-1,-1),0)
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(10,5),50)
+        self.assertEqual(calc.multiply(-1,1),-1)
+        self.assertEqual(calc.multiply(-1,-1),1)
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(10,5),2)
+        self.assertEqual(calc.divide(-1,1),-1)
+        self.assertEqual(calc.divide(-1,-1),1)   
  
 
 if __name__ == '__main__':
      unittest.main()
-#run python test_calc in terminal
-#or within the editor 
-
+#run python test_calc.py in terminal
+#or within the editor
